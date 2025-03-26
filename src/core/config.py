793ec13292ef55632c096b846c07e10154a98a16
Patriotic20 <pterest160@gmail.com>
2025-03-26 15:53:37 +0500

    Fixed commit

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,6 @@
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
 
+from pydantic_settings import BaseSettings , SettingsConfigDict
 
-load_dotenv()
 
 class Settings(BaseSettings):
     DB_USER: str 
@@ -29,7 +27,6 @@ class Settings(BaseSettings):
         )
 
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
